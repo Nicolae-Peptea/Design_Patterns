5c26918e91d0c56302347ccb8aca49883c9a4216
888HOLDINGS\nicolae.peptea <nicolae.peptea@888holdings.com>
2022-09-14 17:33:51 +0300

    rename files for dependency injection

---
+++ DesignPattern/DependencyInversionPrinciple/ExecuteDependecyInversion.cs
@@ -0,0 +1,20 @@
+﻿
+namespace DesignPattern.DependencyInversionPrinciple
+{
+    internal class ExecuteDependecyInversion
+    {
+       public void ExecuteCode()
+        {
+            Person parent = new Person { Name = "John" };
+            Person firstChild = new Person { Name = "Chris" };
+            Person secondChild = new Person { Name = "MAry" };
+
+            Relashionships relashionships = new Relashionships();
+            relashionships.AddParentAndChild(parent, firstChild);
+            relashionships.AddParentAndChild(parent, secondChild);
+
+            new Research(relashionships);
+
+        }
+    }
+}
--- DesignPattern/DependencyInversionPrinciple/Research.cs
+++ DesignPattern/DependencyInversionPrinciple/Research.cs
@@ -14,21 +14,5 @@ namespace DesignPattern.DependencyInversionPrinciple
                 Console.WriteLine($"John has a child called {person.Name}");
             }
         }
-
-        static void Main(string[] args)
-        {
-            Person parent = new Person { Name = "John" };
-            Person firstChild = new Person { Name = "Chris" };
-            Person secondChild = new Person { Name = "MAry" };
-
-            Relashionships relashionships = new Relashionships();
-            relashionships.AddParentAndChild(parent, firstChild);
-            relashionships.AddParentAndChild(parent, secondChild);
-
-            new Research(relashionships);
-
-        }
-
-
     }
 }
